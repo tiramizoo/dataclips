956bc1a121fb6acf8d4db402c27185688d33c88f
AOneP <49039289+AOneP@users.noreply.github.com>
2023-06-23 08:32:44 +0200

    Remove one of XLSX file download way (#18)

--- javascripts/src/dataclips.js
+++ javascripts/src/dataclips.js
@@ -1,9 +1,5 @@
 import { saveAs } from "file-saver";
 
-ExcelBuilder = require("excel-builder-webpack");
-
-const { Workbook, Builder } = ExcelBuilder;
-
 export default class Dataclips {
   constructor(config, customFormatters, customOptions) {
     let schema = Object.assign({}, config.schema);
@@ -145,108 +141,6 @@ export default class Dataclips {
       });
   }
 
-  downloadXLSX(data, schema, filename, disableSeconds) {
-    function formatZeros(value) {
-      if (value) { // 1:00:53 is returned as { hours: 1, seconds: 53 }
-        return value >= 10 ? value : `0${value}`
-      } else {
-        return '00'
-      }
-    }
-
-    const workbook = Builder.createWorkbook();
-
-    const xlsx_number_formats = {
-      date_formatter: { id: 1, numFmtId: 14 },
-      time_formatter: { id: 2, numFmtId: 21 },
-      datetime_formatter: { id: 3, numFmtId: 22 },
-      duration_formatter: { id: 4, numFmtId: 46 },
-    };
-
-    const stylesheet = workbook.getStyleSheet();
-    stylesheet.fills = [{}, {}]; // reset weirdo default styles
-
-    Object.entries(xlsx_number_formats).forEach(([key, format]) => {
-      stylesheet.masterCellFormats.push(format);
-    });
-
-    const sheet = workbook.createWorksheet();
-
-    const headers = Object.values(schema).map((value) => value.label);
-
-    const rows = [];
-
-    rows.push(headers);
-
-    const minMs = 60 * 1000;
-    const dayMs = 60 * 60 * 24 * 1000;
-
-    data.forEach((item) => {
-      const row = Object.keys(schema).map((key) => {
-        const value = item[key];
-
-        if (value !== null) {
-          const type = schema[key].type;
-
-          switch (type) {
-            case "boolean":
-              return {
-                value: +value,
-              };
-            case "date":
-              return {
-                value: 25569 + Date.parse(value) / dayMs,
-                metadata: { style: xlsx_number_formats.date_formatter.id },
-              };
-            case "time":
-              return {
-                value: value.as("day"),
-                metadata: { style: xlsx_number_formats.time_formatter.id },
-              };
-            case "datetime":
-              if (disableSeconds) {
-                return new Date(value.ts).toLocaleString([], {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit'})
-              } else {
-                return {
-                  value: 25569 + (value.ts + value.offset * minMs) / dayMs,
-                  metadata: { style: xlsx_number_formats.time_formatter.id },
-                };
-              }
-            case "duration":
-              if (disableSeconds) {
-                const hours = value.values["days"] ? value.values["days"] * 24 + value.values["hours"] : value.values["hours"]
-                const minutes = value.values["minutes"]
-                return {
-                  value: formatZeros(hours) + ":" + formatZeros(minutes),
-                };
-              } else {
-                return {
-                  value: value.as("day"),
-                  metadata: { style: xlsx_number_formats.duration_formatter.id },
-                };
-              };
-            default:
-              return value;
-          }
-        } else {
-          return null;
-        }
-      });
-
-      rows.push(row);
-    });
-
-    sheet.setData(rows);
-    workbook.addWorksheet(sheet);
-    return new Promise(function (resolve, reject) {
-      Builder.createFile(workbook, { type: "blob" }).then((blobData) => {
-        const blob = new Blob([blobData], { type: "application/octet-stream" });
-        saveAs(blob, filename);
-        resolve();
-      });
-    });
-  }
-
   downloadCSV(data, schema, filename, disableSeconds) {
     if (data === null || !data.length) {
       return null;
@@ -325,7 +219,6 @@ export default class Dataclips {
       time_zone,
       url,
       fetchDataInBatches,
-      downloadXLSX,
       downloadCSV,
       filters,
       default_filter,
@@ -349,29 +242,8 @@ export default class Dataclips {
       },
       disableSeconds: disable_seconds,
       selectable: selectable,
+      fileName: name,
       controls: {
-        xlsx: {
-          onClick: (e) => {
-            e.stopPropagation();
-
-            const button = e.target;
-            const suggestedFilename = `${name}.xlsx`;
-
-            const filename = prompt("filename", suggestedFilename);
-            if (filename !== null) {
-              button.disabled = true;
-              const data = reactable.getFilteredData();
-              downloadXLSX(data, reactable.getFilteredSchema(), filename, reactable.config.disableSeconds).then(
-                () => {
-                  button.disabled = false;
-                }
-              );
-            }
-          },
-          className: "r-icon-file-excel",
-          key: "xlsx",
-          label: "XLSX",
-        },
         csv: {
           onClick: (e) => {
             e.stopPropagation();
@@ -391,9 +263,8 @@ export default class Dataclips {
               );
             }
           },
-          className: "r-icon-doc-text",
           key: "csv",
-          label: "CSV",
+          label: "Download CSV",
         },
         refresh: {
           onClick: (e) => {
@@ -401,7 +272,6 @@ export default class Dataclips {
             reactable.clearData();
             fetch.apply(this);
           },
-          className: "r-icon-arrows-cw",
           key: "refresh",
           label: "Refresh",
         },
